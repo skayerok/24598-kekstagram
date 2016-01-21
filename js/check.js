bd66e93568d8bd414e59abe635a442307611edaa
Pavel Botsman <pavel-botsman@yandex.ru>
2016-01-21 21:39:42 +0300

    переписал функцию с использованием switch и внес правки

--- js/check.js
+++ js/check.js
@@ -1,28 +1,45 @@
 function getMessage(a, b) {
+  var typeA = typeof(a);
+  var typeB = typeof(b);
 
-  if (typeof(a) == "boolean") {
-    if (a == true) {
-      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
 
-    } else if (a == false) {
-      return "Переданное GIF-изображение не анимировано";
-    }
+  switch (typeA) {
 
-  } else if (typeof(a) == "number") {
-    return "Переданное SVG-изображение содержит " + a + " объектов и "+ b * 4 + " аттрибутов";
 
-  } else if (typeof(a) == "object" && typeof(b) != "object") {
-    var sum = 0;
-    for (i = 0; i < a.length; i++) {
-      sum += a[i];
-    }
-    return "Количество красных точек во всех строчках изображения: " + sum;
+    case "boolean":
+      if (a == true) {
+        console.log("case1.1");
+        return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      } else if (a == false) {
+        console.log("case1.2");
+        return "Переданное GIF-изображение не анимировано";
+      }
+      break;
 
-  } else if (typeof(a) == "object" && typeof(b) == "object") {
-    var square = 0;
-    for (i = 0; i < a.length; i++) {
-      square += a[i] * b[i];
-    }
-    return "Общая площадь артефактов сжатия: " + square + " пикселей";
+
+    case "number":
+      return "Переданное SVG-изображение содержит " + a + " объектов и "+ b * 4 + " аттрибутов";
+      break;
+
+
+    case "object":
+      if (Array.isArray(a) && !Array.isArray(b)) {
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+          sum += a[i];
+        }
+        return "Количество красных точек во всех строчках изображения: " + sum;
+      }
+
+
+    case "object":
+      if (Array.isArray(a) && Array.isArray(b)) {
+        var square = 0;
+        for (var i = 0; i < a.length; i++) {
+          square += a[i] * b[i];
+        }
+        return "Общая площадь артефактов сжатия: " + square + " пикселей";
+      }
+      break;
   }
 };
